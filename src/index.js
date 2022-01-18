9b9bce67c6b6bace076991eb8326b74356e78b37
Jayashakthi28 <vishnu28.js@gmail.com>
2022-01-18 08:41:57 +0530

    added react router logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import {App} from './App';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/country/:country" element={<App/>} />
+        <Route path="/india" element={<App/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
