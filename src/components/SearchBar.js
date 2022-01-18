c4dbfab458251ab356be7d7b0da9a803fac6bea4
Jayashakthi28 <vishnu28.js@gmail.com>
2022-01-18 20:56:13 +0530

    added search logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,29 @@
+import { useRef, useState } from 'react';
 import {BsSearch} from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
+
+function searchHandle(e,setsearchState,ref){
+    if(!e.key){
+        setsearchState(e.target.value);
+    }
+    else if(e.key==="Enter"){
+        ref.current.click();
+    }
+}
+
 function SearchBar(){
+    const [searchState,setsearchState]=useState("");
+    const ref=useRef();
     return(
         <div className="searchbar">
-            <input type="text" placeholder="Enter a city"/>
-            <BsSearch className='searchicon'/>
+            <input type="text" value={searchState} placeholder="Enter a country" onChange={(e)=>{
+                searchHandle(e,setsearchState,ref);
+            }} onKeyDown={(e)=>{
+                searchHandle(e,setsearchState,ref)}}/>
+            <BsSearch className='searchicon' onClick={()=>{
+                ref.current.click();
+            }}/>
+            <NavLink ref={ref} to={`/country/${searchState}`}></NavLink>
         </div>
     )
 }
