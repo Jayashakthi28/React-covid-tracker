f9c214c1d88402ea95b4079eccce9feee62ec4f3
Jayashakthi28 <vishnu28.js@gmail.com>
2022-01-18 08:42:21 +0530

    added useContext logic

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Nav from './components/Nav';
 import MainDet from './components/MainDet';
 import SearchBar from './components/SearchBar';
 import CountryCont from './components/CountryCont';
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { animateScroll as scroll } from 'react-scroll';
+import { useParams,useLocation } from 'react-router-dom';
 
-function App() {
+export const ApiContext=React.createContext();
+
+export function App() {
   const [isDataFetch,setisDataFetch]=useState(false);
   const [apiData,setapiData]=useState({});
-  // const [currData,setcurrData]=useState('');
-
+  const location=useLocation().pathname;
+  const navRender=location.search(/^\/india*/gi);
+  
   useEffect(()=>{
     async function fetchData(){
       let {data}=await axios.get('https://api.covid19api.com/summary');
@@ -18,23 +23,29 @@ function App() {
       data.Global.CountryCode="Overall";
       setapiData({
         countries:data["Countries"],
-        curr:data["Global"],
+        global:data["Global"],
         date:data["Date"]
       });
       setisDataFetch(true);
     }
     fetchData();
   },[]);
+  useEffect(()=>{
+    scroll.scrollToTop({duration:500});
+    console.log("Use Params");
+  },[useParams()]);
+  
   console.log(apiData);
+  
   return (
     isDataFetch &&
     <div className="App">
-      <Nav/>
-      <MainDet data={apiData.curr}/>
-      <SearchBar/>
-      <CountryCont data={apiData.countries} setter={{setapiData,apiData}}/>      
+      <ApiContext.Provider value={apiData}>
+        <Nav/>
+        <MainDet/>
+        <SearchBar/>
+        {(navRender===0)?'':<CountryCont data={apiData.countries} setter={{setapiData,apiData}}/> }     
+      </ApiContext.Provider>
     </div>
   );
 }
-
-export default App;
